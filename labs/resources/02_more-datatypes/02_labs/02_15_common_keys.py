ae1fa4a648be67be10ac5a1e7069385b2efeeae8
franpanteli <francescapanteli@gmail.com>
2025-09-01 19:40:05 +0100

    This commit pushes the latest change to the 02_15.py file

--- labs/resources/02_more-datatypes/02_labs/02_15_common_keys.py
+++ labs/resources/02_more-datatypes/02_labs/02_15_common_keys.py
@@ -9,3 +9,36 @@
 
 dict_1 = {"a": 1, "b": 2, "c": 3}
 dict_2 = {"a": 2, "c": 4 , "d": 2}
+
+#separate the dictionary one keys from values in separate lists
+dict_1_keys = []
+dict_1_values = []
+
+for i in dict_1:
+    dict_1_values.append(dict_1[i])
+    dict_1_keys.append(i)
+
+print("dict_1_keys: ", dict_1_keys)
+print("dict_1_values: ", dict_1_values)
+
+#do the same with the second dictionary
+dict_2_keys = []
+dict_2_values = []
+
+for i in dict_2:
+    dict_2_keys.append(i)
+    dict_2_values.append(dict_2[i])
+
+print("dict_2_keys: ", dict_2_keys)
+print("dict_2_values: ", dict_2_values)
+
+
+
+
+
+
+
+
+
+
+
