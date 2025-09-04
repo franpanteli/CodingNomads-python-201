a447295de69d166eca096d10c789b85ae87b77e1
franpanteli <francescapanteli@gmail.com>
2025-09-04 19:38:54 +0100

    This commit pushes changes to the 03_03_writing.py file.

--- labs/resources/03_file-input-output/03_labs/03_03_writing.py
+++ labs/resources/03_file-input-output/03_labs/03_03_writing.py
@@ -1,2 +1,33 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+
+#OPEN THE FILE AND READ INPUT FILE CONTENTS
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+file = open('words.txt', 'r')
+contents = file.read()
+split_contents = contents.split()
+
+#REVERSE THE LIST CONTENTS
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+reversed_list = []
+# for i in split_contents:
+# print(split_contents)
+for index, value in enumerate(split_contents):
+    reversed_list.append(split_contents[len(split_contents)-1-index])
+
+#reversed_list is a list of the words which the original list contains, but backwards
+
+#CLOSE THE INPUT FILE
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+file.close()
+
+words_reverse_txt = open("words_reverse.txt", "w")
+
+for i in reversed_list:
+    words_reverse_txt.write(i)
+    words_reverse_txt.write("\n")
+
+words_reverse_txt.close()
