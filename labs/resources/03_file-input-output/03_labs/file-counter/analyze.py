fe995f2c02a9fa313673463896d80145e49fae23
franpanteli <francescapanteli@gmail.com>
2025-09-04 20:26:40 +0100

    This commit pushes changes to my solutions for the third module in the course."

--- labs/resources/03_file-input-output/03_labs/file-counter/analyze.py
+++ labs/resources/03_file-input-output/03_labs/file-counter/analyze.py
@@ -1,1 +1,8 @@
 # Use the `csv` module to read in and count the different file types.
+import csv
+
+#open the file and read in the data
+#store the data in the contents variable
+filecounts_txt = open('filecounts.txt', 'r')
+contents = filecounts_txt.read()
+filecounts_txt.close()
--- labs/resources/03_file-input-output/03_labs/file-counter/csvwrite.py
+++ labs/resources/03_file-input-output/03_labs/file-counter/csvwrite.py
@@ -1,1 +1,24 @@
 # Write the file counts to a `.csv` file.
+
+#CONTENTS OF THE analyze.py file
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+import csv
+
+#open the file and read in the data
+#store the data in the contents variable
+filecounts_txt = open('filecounts.txt', 'r')
+contents = filecounts_txt.read()
+filecounts_txt.close()
+
+#TO WRITE THE FILE COUNTS TO A .CSV FILE
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#open the csv file
+filecounts_csv = open('filecounts.csv', 'w')
+filecounts_csv.write(contents[1:len(contents)-1])
+    #this is to write the contents of the txt file into the csv file
+    #the indexing is to get rid of the { and }'s in the csv file
+filecounts_csv.close() #close the csv file
