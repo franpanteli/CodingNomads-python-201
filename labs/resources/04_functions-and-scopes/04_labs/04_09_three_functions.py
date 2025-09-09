2e38eba2dec4c997461f84fa65876a62452cc62b
franpanteli <francescapanteli@gmail.com>
2025-09-09 15:25:04 +0100

    This commit pushes my answers to questions in the fourth module in the course.

--- labs/resources/04_functions-and-scopes/04_labs/04_09_three_functions.py
+++ labs/resources/04_functions-and-scopes/04_labs/04_09_three_functions.py
@@ -3,3 +3,19 @@
 # of that function to do something with it. You can have more
 # than three functions, and they don't need to call each other
 # in a circular way.
+
+def uppercase(input_string):
+    return input_string.upper()
+
+def lowercase(input_string):
+    value = uppercase(input_string) #use the uppercase function in the lowercase function
+    return value.lower()
+
+def add_dot(input_string):
+    value = lowercase(input_string)
+    return value + '.'
+
+print(add_dot("HELLO")) #the output is lowercase, no matter what the input is
+    #a dot is added to the end of the input function
+
+
--- labs/resources/04_functions-and-scopes/04_labs/04_10_type_annotations.py
+++ labs/resources/04_functions-and-scopes/04_labs/04_10_type_annotations.py
@@ -1,12 +1,18 @@
 # Add type annotations to the three functions shown below.
+"""Syntax:
+    def greet(greeting: str, name: str) -> str:
+        """Generates a greeting."""
+        sentence = f"{greeting}, {name}! How are you?"
+        return sentence
+"""
 
-def multiply(num1, num2):
+def multiply(num1: float, num2: float) -> float:
     return num1 * num2
 
-def greet(greeting, name):
+def greet(greeting: str, name: str) -> str:
     sentence = f"{greeting}, {name}! How are you?"
     return sentence
 
-def shopping_list(*args):
+def shopping_list(*args: str) -> tuple:
     [print(f"* {item}") for item in args]
     return args
