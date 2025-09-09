a68102e3727bff7be2594e258947bfea23efeca2
franpanteli <francescapanteli@gmail.com>
2025-09-09 18:30:40 +0100

    This commit pushes changes to the my_enumerate() function, which can be used to replace the enumerate() function in a list of tuples

--- labs/projects/making_the_enumerate_function.py/my_enumerate.py
+++ labs/projects/making_the_enumerate_function.py/my_enumerate.py
@@ -13,20 +13,50 @@
 """
 
 list = ['Intro', 'Intermediate', 'Advanced', 'Professional']
+# [(0, 'Intro'), (1, 'Intermediate'), (2, 'Advanced'), (3, 'Professional')]
 
-def my_enumerate(list):  # add your arguments
-    #   add your code implementation
-    #   pass
-    index = 0
+
+"""What we want to define
+    -> a function which returns a list of tuples
+    -> the first element of the tuple contains the index
+    -> the second element of the tuple contains the value of the list which we are iterating through
+    -> the enumerate function does this:
+
+0 Intro
+1 Intermediate
+2 Advanced
+3 Professional
+
+    -> we want the my_enumerate function to do the same
+    -> we want to write a function called my_enumerate() function which takes this:
+
+    list = ['Intro', 'Intermediate', 'Advanced', 'Professional']
+
+    and converts it into this:
+
+    [(0, 'Intro'), (1, 'Intermediate'), (2, 'Advanced'), (3, 'Professional')]
+    """
+
+# this is a function which takes a list and returns another list of this format:
+    # [(0, 'Intro'), (1, 'Intermediate'), (2, 'Advanced'), (3, 'Professional')]
+def my_enumerate(list):
+    index_counter = 0
+    return_list = []
     for i in list:
-        print((index,i))
-        index += 1
+        return_list.append((index_counter, str(i)))
+        index_counter += 1
+    return return_list
 
-my_enumerate(list)
-
+# print(my_enumerate(list))
 for index, value in my_enumerate(list):
-    print("Index:", index)
-    print("Value:", value)
+    print("Index:",index)
+    print("Value:",value)
+
+#the my_enumerate function is about taking a list and converting it into a list of this format:
+# ['Intro', 'Intermediate', 'Advanced', 'Professional']
+# [(0, 'Intro'), (1, 'Intermediate'), (2, 'Advanced'), (3, 'Professional')]
+#the my_enumerate() function can then be used in the same way as the enumerate() function
+#this is an array of tuples
 
 
 
