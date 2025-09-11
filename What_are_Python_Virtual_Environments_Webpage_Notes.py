f0a5c7c9b1183a1caae9fb9948daef2325fcb185
franpanteli <francescapanteli@gmail.com>
2025-09-11 18:43:04 +0100

    This commit pushes a set of webpage notes

--- What_are_Python_Virtual_Environments_Webpage_Notes.py
+++ What_are_Python_Virtual_Environments_Webpage_Notes.py
@@ -1,3 +1,19 @@
 """What are Python Virtual Environments webpage notes
     -> this webpage is on virtual environments
+    -> VMs <- Virtual Machines
+        -> virtual environments
+        -> to run a different operating system on the current one
+        -> virtual environments
+            -> they aren't an entire operating system
+            -> THERE IS A DIFFERENCE BETWEEN A VIRTUAL MACHINE AND A VIRTUAL ENVIRONMENT
+                -> VIRTUAL MACHINES HAVE ENTIRE OPERATING SYSTEMS ON THEM
+                -> a virtual environment is used to gain separation between the project and the computer which it's hosted on
+            -> example
+                -> we need a folder for the project and its packages
+                -> there is a folder structure for this on Windows and Mac
+                -> you don't have to build the virtual environment yourself (there is a command for this)
+    -> summary
+        -> software development is made safer by virtual environments
+        -> THE PROJECT HAS ITS OWN FOLDER
+        -> creating a virtual environment <- next
     """
