5ac4a2426a93a013720b1aba610458dbde41c2ae
franpanteli <francescapanteli@gmail.com>
2025-09-13 22:32:40 +0100

    This commit pushes the first half of module 5 exercises.

---
+++ labs/resources/05_venvs-and-packages/05_labs/django_02/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/francescapanteli/Desktop/CodingNomads-python-201/labs/resources/05_venvs-and-packages/05_labs/django_02/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
