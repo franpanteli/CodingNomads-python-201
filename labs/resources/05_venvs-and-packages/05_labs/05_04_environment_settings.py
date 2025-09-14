140dd6d9e52068ddb82fee49db8522c7c18be0b1
franpanteli <francescapanteli@gmail.com>
2025-09-14 20:33:31 +0100

    This commit pushes changes to the exercises in the fifth module of the course. The exercises are now completed (final changes to the files in this directory.

--- labs/resources/05_venvs-and-packages/05_labs/05_04_environment_settings.py
+++ labs/resources/05_venvs-and-packages/05_labs/05_04_environment_settings.py
@@ -1,8 +1,18 @@
 # Create a virtual environment and edit the activation script to add
+    -> the activation script is in the bin folder
+    -> bin > activate
+
 # the following information:
 # 
 # - ENVIRONMENT="development"
 # - SECRET="i ate your sweets"
-# 
+    -> this is added into the activate file in the bin directory
+    -> export and then the names and values of those variables
+
 # Then write the necessary code to access and print the values of these
 # two environment variables in this script.
+
+Python -m venv virtual_environment_for_05_04
+
+printenv
+    -> to print the environment variables
