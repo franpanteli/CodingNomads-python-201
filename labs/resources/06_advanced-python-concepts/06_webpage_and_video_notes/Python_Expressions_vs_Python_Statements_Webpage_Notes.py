74a12571684fe79df4b85087011dec382b5233df
franpanteli <francescapanteli@gmail.com>
2025-09-15 22:59:05 +0100

    This commit pushes the latest updates to module six notes

--- labs/resources/06_advanced-python-concepts/06_webpage_and_video_notes/Python_Expressions_vs_Python_Statements_Webpage_Notes.py
+++ labs/resources/06_advanced-python-concepts/06_webpage_and_video_notes/Python_Expressions_vs_Python_Statements_Webpage_Notes.py
@@ -1,5 +1,12 @@
 """Python Expressions vs Python Statements Webpage Notes
     -> outline
-        ->  
-    ->
+        -> Python expressions and statements
+        -> expressions and statements
+    -> expressions ans statements
+        -> these terms are frequently used
+    -> Python expresssions
+        -> programming logic, which evaluates to a result
+        -> for example, adding two numbers together
+    -> Python statements
+        -> EXPRESSIONS EVALUATE TO SOMETHING, AND STATEMENTS
 """
