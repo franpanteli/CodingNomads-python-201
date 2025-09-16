ba5c08e91c523f0c60788e4238feeb4568d3ad3b
franpanteli <francescapanteli@gmail.com>
2025-09-16 18:56:37 +0100

    This commit pushes changes to module six notes

--- labs/resources/06_advanced-python-concepts/06_webpage_and_video_notes/Python_Expressions_vs_Python_Statements_Webpage_Notes.py
+++ labs/resources/06_advanced-python-concepts/06_webpage_and_video_notes/Python_Expressions_vs_Python_Statements_Webpage_Notes.py
@@ -2,11 +2,26 @@
     -> outline
         -> Python expressions and statements
         -> expressions and statements
-    -> expressions ans statements
+    -> expressions and statements
         -> these terms are frequently used
     -> Python expresssions
         -> programming logic, which evaluates to a result
         -> for example, adding two numbers together
+        -> the expression is the operation that we want to calculate
     -> Python statements
         -> EXPRESSIONS EVALUATE TO SOMETHING, AND STATEMENTS
+        -> programs are made of multiple statements
+        -> statements can be made of statements and other expressions
+        -> an example øf this is x = 2
+        -> EXPRESSIONS TELL IT TO CALCULATE SOMETHING
+            -> one has an expected value, the other does not
+    -> expressions and statements in a program
+        -> expressions and statements are used together in a program
+        -> x = 2 + 3 <- example statement with expression
+            -> statement (LHS), expression (RHS)
+            -> the entire thing is a statement
+            -> you should research questions which you have
+            -> STATEMENTS ARE USEFUL WHEN PASSING ARGUMENTS INTO FUNCTIONS
+    -> summary
+        -> EXPRESSIONS EVALUATE TO A RESULT, STATEMENTS DON'T
 """
