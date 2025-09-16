c6b565c2d172e4b853fd4cbad1681c2ab6af4b7c
franpanteli <francescapanteli@gmail.com>
2025-09-16 20:16:07 +0100

    This commit pushes first third of module six solutions.

--- labs/resources/06_advanced-python-concepts/06_labs/06_06_shirt_shop.py
+++ labs/resources/06_advanced-python-concepts/06_labs/06_06_shirt_shop.py
@@ -3,3 +3,25 @@
 
 colors = ["neon orange", "spring green"]
 sizes = ["S", "M", "L"]
+
+# desired output (cartesian product)
+# [
+#   ("neon orange", "S"),
+#   ("neon orange", "M"),
+#   ("neon orange", "L"),
+#   ("spring green", "S"),
+#   ("spring green", "M"),
+#   ("spring green", "L")
+# ]
+
+#the same thing done with a for loop
+# list = []
+# for i in colors:
+#     for j in sizes:
+#         list.append((i,j))
+# print(list)
+
+#the same thing done with a list comprehension
+    #to embed a for loop inside another loop, we use for i in list_name for j in other_list_name
+output_list = [(i,j) for i in colors for j in sizes]
+print(output_list)
