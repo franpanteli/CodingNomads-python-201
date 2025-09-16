cccf67252e00d5dac3ac2116f6649f817e21ab9d
franpanteli <francescapanteli@gmail.com>
2025-09-16 21:00:57 +0100

    This commit pushes first third of module six solutions.

--- labs/resources/06_advanced-python-concepts/06_labs/06_15_baby_names.py
+++ labs/resources/06_advanced-python-concepts/06_labs/06_15_baby_names.py
@@ -6,3 +6,25 @@
 # to map all the baby names that begin with a 'M' to an output list.
 
 names = ['Olivia', 'Noah', 'Ava', 'Oliver', 'Isabella', 'Mason', 'Sophia', 'Logan', 'Emma', 'Liam', 'Amelia', 'Lucas', 'Mia', 'Elijah', 'Charlotte', 'Ethan', 'Harper', 'James', 'Mila', 'Aiden', 'Aria', 'Carter', 'Ella', 'Jackson', 'Evelyn', 'Alexander', 'Avery', 'Sebastian', 'Abigail', 'Michael', 'Emily', 'Benjamin', 'Luna', 'Jacob', 'Riley', 'William', 'Scarlett', 'Grayson', 'Chloe', 'Jack', 'Sofia', 'Daniel', 'Layla', 'Owen', 'Lily', 'Luke', 'Madison', 'Henry', 'Ellie', 'Wyatt', 'Zoey', 'Jayden', 'Elizabeth', 'Leo', 'Penelope', 'Gabriel', 'Victoria', 'Julian', 'Grace', 'Matthew', 'Nora', 'David', 'Aubrey', 'Jaxon', 'Camila', 'Levi', 'Hannah', 'Mateo', 'Bella', 'Asher', 'Aurora', 'Lincoln', 'Addison', 'John', 'Stella', 'Samuel', 'Skylar', 'Muhammad', 'Paisley', 'Ryan', 'Savannah', 'Adam', 'Maya', 'Isaac', 'Natalie', 'Nathan', 'Elena', 'Josiah', 'Emilia', 'Isaiah', 'Violet', 'Joseph', 'Hazel', 'Caleb', 'Nova', 'Anthony', 'Niamey', 'Hunter', 'Eva', 'Eli']
+# filter(names, key = lambda x: x.startswith[0])
+
+print(list(filter(lambda x: x[0]=="M", names)))
+
+"""
+    -> we are printing out the result (first method)
+    -> converting the generator object into a list (second method)
+    -> the third method is the filter method 
+        -> the arguments of this are the list of names we want to sort (last, `names`)
+        -> the first argument of this is the lambda function which we want to sort the elements by 
+            -> it is a function, of x in the list
+            -> x represents the name in the list
+            -> it has to return as a True statement 
+            -> the condition for which the function returns as True 
+            -> without x in front of the lambda function, then whatever is entered after the colon is returned by the function 
+            -> this can be returned if the function is stored in a variable 
+                -> but they are still called anonymous functions, because the def keyword with the name of the function isn't used 
+"""
+
+#example from webpage
+# squares = list(filter(lambda x: x**2, range(10))) #replace the map method with the filter method
+# print(squares)
