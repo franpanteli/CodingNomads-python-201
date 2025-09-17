ef843da5dfbf77bb9b25fb5469240fbb97c202e3
franpanteli <francescapanteli@gmail.com>
2025-09-17 18:03:02 +0100

    This commit pushes changes to the webpage notes in module 7.

--- labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/using_SQLAlchemy_JOINS.py
+++ labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/using_SQLAlchemy_JOINS.py
@@ -43,5 +43,33 @@ pprint(result_set) #the output from this is hundreds of lines that look like thi
 
 """
     -> JOIN queries video notes 
-        -> 
+        -> performing a join statement, rather than just selecting from one table 
+        -> selecting all the actors and all the films they were in 
+        -> selecting a specific actor and the films they were in 
+        -> we join the actor table with the film actor table, then the film actor table with the film table 
+            -> WE ARE MAKING MULTIPLE JOINS
+            -> THERE ARE TWO JOIN STATEMENTS REQUIRED FOR THIS 
+        -> TABLE OBJECTS ARE FIRST CREATED FOR THIS 
+        -> then creating the join statement 
+            -> this is what is used to set up the query 
+            -> we will select the film names and then the actor names 
+        -> starting by using one table 
+            -> actor.join(film_actor, film_actor.columns.actor_id == actor.columns.actor_id)))
+                -> we are joining the film actor object
+                -> WE ARE JOINING THE TWO TABLES ON A CONDITION, THAT THE ACTOR ID MATCHES THE FILM ID 
+                -> this joins the film actor table with the actor table 
+        -> then joining this with the film table 
+            -> this is done by using the join_actor_film_actor varible 
+            -> this variable is then joined with another join statement in SQLAlchemy 
+            -> we are joining the object we just created with the film object
+                -> joining the film object on the condition that the IDs in the two tables match 
+                -> WHENEVER JOINS ARE PERFORMED, AND MULTIPLE JOINS CAN BE USED, TWO TABLES ARE ALWAYS JOINED ON SOME STATEMENT 
+                -> sqlalchemy.select()
+                    -> we are selecting specific collumns from the join statement 
+                    -> we are selecting from columns which we write in the code
+                    -> there are the columns we want to select, and then there is where we want to select them from 
+        -> then creating the result proxy, the result set and printing out the result set
+            -> fetching the results and pretty printing them 
+            -> we get the first name of the actor, in this case, and all of the films they are in
+            -> it does this for all of the actors in the table 
 """
