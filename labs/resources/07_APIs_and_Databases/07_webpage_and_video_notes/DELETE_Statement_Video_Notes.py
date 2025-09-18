279d49405f47f9cbca654b43c64a430fb044e702
franpanteli <francescapanteli@gmail.com>
2025-09-18 20:57:17 +0100

    This commit pushes changes to the webpage notes in module 7.

--- labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/DELETE_Statement_Video_Notes.py
+++ labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/DELETE_Statement_Video_Notes.py
@@ -1,15 +1,24 @@
 """DELETE Statement
-    -> 
+    -> the delete statement
+    -> deleting one or more records from a table
+        -> this is done using the `delete` and `where` keywords
+        -> this is used to delete all records from the table newTable, where the salary column < 100,000
+    -> he changes the code he wrote in the previous section
+    -> deleting, rather than updating, methods
+    -> deleting everything where the salary is < 100,000, and then executing this query
+    -> he looks into the database and this shows the element was deleted
 """
 
 # import modules
 import sqlalchemy
 
-engine = sqlalchemy.create_engine('mysql+pymysql://username:password@localhost/sakila')
-connection = engine.connect()
-metadata = sqlalchemy.MetaData()
+engine = sqlalchemy.create_engine('mysql+pymysql://username:password@localhost/sakila') #create an engine to connect to the Sakila database
+connection = engine.connect() #connect to the Sakila database
+metadata = sqlalchemy.MetaData() #record object metadata
 
-newTable = sqlalchemy.Table('newTable', metadata, autoload=True, autoload_with=engine)
+newTable = sqlalchemy.Table('newTable', metadata, autoload=True, autoload_with=engine) #create a new table, called newTable
 
-query = sqlalchemy.delete(newTable).where(newTable.columns.salary < 100000)
-results = connection.execute(query)
+query = sqlalchemy.delete(newTable).where(newTable.columns.salary < 100000) #use the delete method to delete columns in the table we just created, on the condition that the salary is less than 100,000
+results = connection.execute(query) #execute the query we just defined
+
+
---
+++ labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/SQLAlchemy_Database_Integration_Summary_Webpage_Notes.py
@@ -0,0 +1,13 @@
+"""
+    -> Python can be used with the SQLAlchemy module, to use SQL with Python
+    -> this is used to access information and change database objects in Python
+    -> this can be used to integrate with relational databases
+        -> connecting to a database
+        -> querying databases
+        -> filtering the result
+        -> creating tables
+        -> inserting records
+        -> updating records
+        -> deleting records
+    -> there is another SQL course which can be referred to for this
+"""
