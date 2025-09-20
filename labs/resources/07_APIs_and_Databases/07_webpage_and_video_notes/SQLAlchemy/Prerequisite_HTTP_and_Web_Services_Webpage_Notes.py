a9c518c77c60b39a65f43c2b6f3b083ab516468b
franpanteli <francescapanteli@gmail.com>
2025-09-20 16:17:24 +0100

    This commit pushes changes to the module 7 notes in the course.

---
+++ labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/SQLAlchemy/Prerequisite_HTTP_and_Web_Services_Webpage_Notes.py
@@ -0,0 +1,10 @@
+"""Prerequisite HTTP and web services webpage notes
+    -> outline
+        -> introduction
+        -> web services
+        -> JavaScript Object Notation (JSON)
+        -> Hypertext Transfer Protocol (HTTP)
+        -> REST APIs
+    -> JSON STANDS FOR JAVASCRIPT OBJECT NOTATION
+    -> there are a lot of different reading materials
+"""
