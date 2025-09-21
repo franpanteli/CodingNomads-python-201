3777ba3f42bf4ae64ce39d7b10acafbb93f92be7
franpanteli <francescapanteli@gmail.com>
2025-09-21 16:56:43 +0100

    This commit pushes changes to the seventh module exercise files.

--- labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/REST_API_Integration_with_Python_requests_Package/Make_a_POST_Request.py
+++ labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/REST_API_Integration_with_Python_requests_Package/Make_a_POST_Request.py
@@ -1,3 +1,24 @@
+"""Make a POST Request Webpage Notes
+    -> POST requests can send data to the server
+    -> THIS IS DONE BY USING THE .post() METHOD
+    -> to pass information into the POST request, we use the JSON parameter
 """
+
+import requests #import the requests module
+
+#store the url we want to make the api request from in another variable
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+#STORE THE DATA WE WANT TO POST TO THE API ENDPOINT IN A DICTIONARY, WITH KEY-VALUE PAIRS
+body = {
+    "first_name": "[YOUR FIRST NAME FIELD]",#add first name inside quotes
+    "last_name": "[YOUR LAST NAME FIELD]",#add last name inside quotes
+    "email": "[YOUR EMAIL FIELD]"#add email inside quotes
+}
+
+#use the .post() method to send the information stored in the base_url dictionary to the API
+response = requests.post(base_url, json=body)
+
+"""Video Notes
     -> 
 """
