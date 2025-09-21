ba74a7b85b62cc880f3f8480e287d9d6d2eae89e
franpanteli <francescapanteli@gmail.com>
2025-09-21 15:04:29 +0100

    This commit pushes changes to the seventh module exercise files.

--- labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/REST_API_Integration_with_Python_requests_Package/query_parameters.py
+++ labs/resources/07_APIs_and_Databases/07_webpage_and_video_notes/REST_API_Integration_with_Python_requests_Package/query_parameters.py
@@ -1,3 +1,38 @@
+"""Query parameters
+    -> a part of the URL or as a pair and argument
+    -> like a WHERE clause for SQL
+    -> THE QUERY PARAMETERS ARE KEY-VALUE PAIRS
+        -> as a part of the URL
+        -> ? IN THE URL INDICATES A QUERY PARAMETERS
+        -> WE ARE ASKING THE SERVER FOR INFORMATION
+        -> e.g http://demo.codingnomads.co:8080/tasks_api/tasks?userId=1&complete=true
+            -> userID and complete
+            -> THE QUERY PARAMETERS LIST STARTS WITH A ?, AND THE QUERY PARAMETERS ARE SEPARATED BY AN &
+            -> THE RESPONSE OBJECT IN THIS EXAMPLE IS A JSON FILE, JAVASCRIPT OBJECT NOTATION
+    -> QUERY PARAMETERS CAN BE PASSED INTO THE URL, OR AS A KEY-VALUE PAIR IN THE REQUEST
 """
 
+import requests #import the requests module
+
+response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users?email=glillemani@bandcamp.com")
+    #use the requests module and the get method
+    #we are retrieving information from this api (with the link)
+
+print(response.status_code) #print the status code of this request
+
+# is the same as
+
+params = { #store the parameters in a dictionary
+    "email": "glillemani@bandcamp.com"
+}
+
+response = requests.get("http://demo.codingnomads.co:8080/tasks_api/users", params=params)
+    #this is the same as using a get method on a shorter url, with everything as before the question mark
+
+print(response.status_code)
+"""
+    -> they are key value pairs that exist in the url 
+    -> you can print out the json response of the call 
+    -> THE QUERY PARAMETERS CAN BE TAKEN OUT AND PUT IN A DICTIONARY 
+    -> THIS DICTIONARY IS THEN PASSED AS THE SECOND ARGUMENT (FOR params) IN A GET REQUEST
 """
