eb368c98760ea533a06cebccd20dad752975206c
franpanteli <francescapanteli@gmail.com>
2025-10-05 21:14:10 +0100

    This commit pushes changes to module 8 notes.

---
+++ labs/resources/08_debugging/08_webpage_and_video_notes/What_is_the_Python_Debugger_Webpage_Notes.py
@@ -0,0 +1,13 @@
+"""What is the Python debugger
+    -> outline
+        -> introduction
+        -> more code, more problems
+            -> why use the debugger
+    -> we have previously used the print function
+        -> this can be used to print the value of variables, at different points in the execution of the code
+        -> this is harder to do for larger applications
+            -> there are more errors the more code we have
+            -> larger programs are more likely to have errors in them
+        -> THERE ARE ENTIRE LIBRARIES JUST FOR DEBUGGING THE CODE
+    -> breakpoint() <- to add check-in points to the code, to use to inspect the script  
+"""
