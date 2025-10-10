6f964a5b309a210bdb911c917f72a502b0a7bf24
franpanteli <francescapanteli@gmail.com>
2025-10-10 16:58:02 +0100

    This commit pushes changes to exercises in module 8

---
+++ labs/resources/09_Integrating_APIs/API_Project_Another_API_Webpage_Notes.py
@@ -0,0 +1,52 @@
+"""API Project Another API Webpage Notes
+    -> outline
+        -> introduction
+        -> possible API ideas
+    -> this is in the dungeons_and_dragon_game_4.0_with_APIs directory in the projects folder (of this repository)
+        -> this repository > projects > dungeons_and_dragon_game_4.0_with_APIs
+    -> we have already put a random name generator into this project
+        -> this was done using another API request
+    -> this is ideas for other APIs we can add to it
+    -> a lot of information is on the internet through free APIs
+    -> we can disregard their ideas and use our own
+    -> API ideas
+        -> weather information
+            -> based on the weather of the players
+            -> allowing them to find different items in the game based on the weather
+            -> we can just get the weather in their location and print it to them
+        -> location information
+            -> using location information with a real-time weather API
+            -> asking the players where they are, then using their response to inclufence gameplay
+        -> name meanings
+            -> looking up the meaning of their real name and using a modifier, e.g The Brave
+        -> word meanings
+            -> ALLOWING THE USERS TO FIND INFORMATION WITH STRANGE NAMES
+            -> looking up the meaning of these words with the API
+        -> recipies
+            -> looking up recipies from the food items collected
+        -> conversations
+            -> showing social media conversations that the players can listen to
+        -> translations
+            -> give players the choice to read the game instructions in different language
+        -> THE EASIEST IS TO PRINT OUT THE WEATHER OF WHERE THEY ACTUALLY ARE
+        -> pick an API that:
+            -> is free to use
+            -> doesn't require authentication
+            -> has nice documentation we can use
+        -> we can also use an API which we have previously
+            -> printing out a famous quote, or a random image of a dog
+            -> either
+                -> use the Weather information of where they are
+                -> give them a random dog image (here is a picture of the opponent:)
+                    -> showing them that they encountered a dog / that their prize is a randmly generated image of
+                        the dog
+                    -> at this url
+                    -> when they win the game, showing them that their prize is a randomly generated image of a dog,
+                        collectable at the url - provided that they have internet
+            -> implementing the API even if it isn't as straight forward as their ideas
+                -> we previously used the Uzby API, to generate a random name for them - of equal length to the
+                    one they entered
+                -> WE DON'T WANT TO PUSH API KEYS TO GITHUB, THIS IS A SECURITY RISK - THEY SHOULD BE IN AN ENVIRONMENT
+                    VARIABLES FOLDER / FILE
+                -> they want us to share this on Discord once done 
+"""
