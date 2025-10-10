b674163ccfc7c8e0b508f1441e5c6a3ed322348a
franpanteli <francescapanteli@gmail.com>
2025-10-10 17:03:05 +0100

    This commit pushes changes to exercises in module 8

--- labs/projects/dungeons_and_dragon_game_4.0_with_APIs/weather-game.py
+++ labs/projects/dungeons_and_dragon_game_4.0_with_APIs/weather-game.py
@@ -1,3 +1,12 @@
-# Include the current weather into your game mechanics.
+#Task:
+    # Include the current weather into your game mechanics.
 
-URL = "https://www.metaweather.com/api/"
+    URL = "https://www.metaweather.com/api/"
+
+"""Notes:
+    -> this API doesn't work 
+    -> instead of this, a dog image generator API was used in the fourth iteration of the project
+    -> this means that when the user wins a battle, they will win a prize, which is a randomly generated image of a 
+        dog 
+    -> this is the same API that was used in the Python_REST_API_Tutorial_(Automation_Tools) project 
+"""
